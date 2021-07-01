3dce67963c7129747ac427140802074442dc556f
fkubota <rkkubotay@gmail.com>
2021-07-01 18:11:09 +0900

    [update] build

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,16 @@
 module.exports = {
-  transpileDependencies: [
-    'vuetify'
-  ]
+  "outputDir": "docs",
+  "assetsDir": "./",
+  "publicPath": "./",
+  "transpileDependencies": [
+    "vuetify"
+  ],
+  
+  pages: {
+    index: {
+      entry: 'src/main.js', // 必須パラメータ
+      title: 'kaggler-slack-emoji',
+      template: 'public/index.html'
+    }
+  },
 }
